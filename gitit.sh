61e5f0dd90d362cf945194d2af8e396485ee332a
rcooke-ast <rcooke.ast@gmail.com>
2020-05-04 19:25:28 +0100

    update

--- gitit.sh
+++ gitit.sh
@@ -1,2 +1,6 @@
+rm BikeWarp/core/assets/BGstate.dat
+rm BikeWarp/core/assets/WorldRecords.dat
+rm BikeWarp/core/assets/replays/*
+rmdir BikeWarp/core/assets/replays
 git commit * -m "update"
 git push
