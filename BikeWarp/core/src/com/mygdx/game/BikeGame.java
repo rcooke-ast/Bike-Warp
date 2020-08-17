306bfbc7d6c79ef02961de5f8606a8ddc2cf5b94
rcooke-ast <rcooke.ast@gmail.com>
2020-08-17 19:36:46 +0100

    update

--- BikeWarp/core/src/com/mygdx/game/BikeGame.java
+++ BikeWarp/core/src/com/mygdx/game/BikeGame.java
@@ -4,14 +4,19 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.handlers.GameInput;
 import com.mygdx.game.handlers.GameInputProcessor;
 import com.mygdx.game.handlers.GameStateManager;
 
 public class BikeGame implements ApplicationListener {
 	public static final String TITLE = "Bike Warp";
+//	public static final int V_WIDTH = 512;//512;//480;
+//	public static final int V_HEIGHT = 320;//384;//360;
 	public static final int V_WIDTH = 512;//480;
 	public static final int V_HEIGHT = 384;//360;
+	private static final float ASPECT_RATIO = (float)V_WIDTH/(float)V_HEIGHT;
 	public static final int SCALE = 2;
 
 	public static final float STEP = 1 / 100f;
@@ -20,6 +25,7 @@ public class BikeGame implements ApplicationListener {
 	private SpriteBatch sb;
 	private OrthographicCamera cam;
 	private OrthographicCamera hudCam;
+	private Rectangle viewport;
 
 	private GameStateManager gsm;
 
@@ -65,7 +71,29 @@ public class BikeGame implements ApplicationListener {
 
 	@Override
 	public void resize (int w, int h) {
-
+		// calculate new viewport
+//		float aspectRatio = (float)w/(float)h;
+//		float scale = 1f;
+//		Vector2 crop = new Vector2(0f, 0f);
+//
+//		if(aspectRatio > ASPECT_RATIO)
+//		{
+//			scale = (float)h/(float)V_HEIGHT;
+//			crop.x = (w - V_WIDTH*scale)/2f;
+//		}
+//		else if(aspectRatio < ASPECT_RATIO)
+//		{
+//			scale = (float)w/(float)V_WIDTH;
+//			crop.y = (h - V_HEIGHT*scale)/2f;
+//		}
+//		else
+//		{
+//			scale = (float)w/(float)V_WIDTH;
+//		}
+//
+//		float wid = (float)V_WIDTH*scale;
+//		float hei = (float)V_HEIGHT*scale;
+//		viewport = new Rectangle(crop.x, crop.y, wid, hei);
 	}
 
 	@Override
