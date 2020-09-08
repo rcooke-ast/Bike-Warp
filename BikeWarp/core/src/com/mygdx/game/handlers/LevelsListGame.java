bd8b03b9d80fa35a9577d4a86a65d84e55142408
rcooke-ast <rcooke.ast@gmail.com>
2020-09-08 15:26:20 +0100

    update

--- BikeWarp/core/src/com/mygdx/game/handlers/LevelsListGame.java
+++ BikeWarp/core/src/com/mygdx/game/handlers/LevelsListGame.java
@@ -26,125 +26,128 @@ public class LevelsListGame {
     	// Level 1
     	gameLevelNames[1] = "1. Start Your Engine";
     	gameLevelFiles[1] = "data/gamelevels/01_StartYourEngine.lvl";
-    	gameLevelTips[1] = "Tip: Collect the gold jewel and head to the exit warp";
+    	gameLevelTips[1] = "Tip: Collect the emerald jewel and head to the exit warp";
     	// Level 2
-    	gameLevelNames[2] = "2. Training";
-    	gameLevelFiles[2] = "data/gamelevels/02_Training.lvl";
+    	gameLevelNames[2] = "2. Same Same But Different";
+    	gameLevelFiles[2] = "data/gamelevels/02_SameSameButDifferent.lvl";
     	gameLevelTips[2] = "Tip: Use the 'b' key to bunny hop";
     	// Level 3
     	gameLevelNames[3] = "3. Hang Tight!";
     	gameLevelFiles[3] = "data/gamelevels/03_HangTight.lvl";
     	// Level 4
-    	gameLevelNames[4] = "4. Same Same But Different";
-    	gameLevelFiles[4] = "data/gamelevels/04_SameSameButDifferent.lvl";
+    	gameLevelNames[4] = "4. Excavation";
+    	gameLevelFiles[4] = "data/gamelevels/04_Excavation.lvl";
     	// Level 5
-    	gameLevelNames[5] = "5. Levitate";
-    	gameLevelFiles[5] = "data/gamelevels/05_Levitate.lvl";
+    	gameLevelNames[5] = "5. Black Mamba";
+    	gameLevelFiles[5] = "data/gamelevels/05_BlackMamba.lvl";
     	// Level 6
-    	gameLevelNames[6] = "6. Excavation";
-    	gameLevelFiles[6] = "data/gamelevels/06_Excavation.lvl";
+    	gameLevelNames[6] = "6. Demolish";
+    	gameLevelFiles[6] = "data/gamelevels/06_Demolish.lvl";
     	// Level 7
-    	gameLevelNames[7] = "7. Demolish";
-    	gameLevelFiles[7] = "data/gamelevels/07_Demolish.lvl";
+    	gameLevelNames[7] = "7. Bumpy Battle";
+    	gameLevelFiles[7] = "data/gamelevels/07_BumpyBattle.lvl";
     	// Level 8
-    	gameLevelNames[8] = "8. Big Tree Monument";
-    	gameLevelFiles[8] = "data/gamelevels/08_BigTreeMonument.lvl";
-    	// Level 9
-    	gameLevelNames[9] = "9. Reservoir";
-    	gameLevelFiles[9] = "data/gamelevels/09_Reservoir.lvl";
+    	gameLevelNames[8] = "8. Cascade";
+    	gameLevelFiles[8] = "data/gamelevels/08_Cascade.lvl";
+		// Level 9
+		gameLevelNames[9] = "9. Kilimanjaro";
+		gameLevelFiles[9] = "data/gamelevels/09_Kilimanjaro.lvl";
+		// Level 9 3/4
+    	gameLevelNames[10] = "9 and 3/4. Levitate";
+    	gameLevelFiles[10] = "data/gamelevels/09-75_Levitate.lvl";
     	// Level 10
-    	gameLevelNames[10] = "10. Bumpy Battle";
-    	gameLevelFiles[10] = "data/gamelevels/10_BumpyBattle.lvl";
+    	gameLevelNames[11] = "10. Upside Downhill";
+    	gameLevelFiles[11] = "data/gamelevels/10_UpsideDownhill.lvl";
     	// Level 11
-    	gameLevelNames[11] = "11. Black Mountain";
-    	gameLevelFiles[11] = "data/gamelevels/11_BlackMountain.lvl";
+    	gameLevelNames[12] = "11. Logging";
+    	gameLevelFiles[12] = "data/gamelevels/11_Logging.lvl";
     	// Level 12
-    	gameLevelNames[12] = "12. The Tip";
-    	gameLevelFiles[12] = "data/gamelevels/12_TheTip.lvl";
+    	gameLevelNames[13] = "12. Tantalising";
+    	gameLevelFiles[13] = "data/gamelevels/12_Tantalising.lvl";
     	// Level 13
-    	gameLevelNames[13] = "13. Cascade";
-    	gameLevelFiles[13] = "data/gamelevels/13_Cascade.lvl";
+    	gameLevelNames[14] = "13. Over The Falls";
+    	gameLevelFiles[14] = "data/gamelevels/13_OverTheFalls.lvl";
     	// Level 14
-    	gameLevelNames[14] = "14. Mount Kilimanjaro";
-    	gameLevelFiles[14] = "data/gamelevels/14_Kilimanjaro.lvl";
+    	gameLevelNames[15] = "14. Trials";
+    	gameLevelFiles[15] = "data/gamelevels/14_Trials.lvl";
     	// Level 15
-    	gameLevelNames[15] = "15. Upside Downhill";
-    	gameLevelFiles[15] = "data/gamelevels/15_UpsideDownhill.lvl";
+    	gameLevelNames[16] = "15. Tony The African Hawk";
+    	gameLevelFiles[16] = "data/gamelevels/15_TonyTheAfricanHawk.lvl";
     	// Level 16
-    	gameLevelNames[16] = "16. Logging";
-    	gameLevelFiles[16] = "data/gamelevels/16_Logging.lvl";
+    	gameLevelNames[17] = "16. Big Tree Monument";
+    	gameLevelFiles[17] = "data/gamelevels/16_BigTreeMonument.lvl";
     	// Level 17
-    	gameLevelNames[17] = "17. Tantalising";
-    	gameLevelFiles[17] = "data/gamelevels/17_Tantalising.lvl";
+    	gameLevelNames[18] = "17. Lava Pits";
+    	gameLevelFiles[18] = "data/gamelevels/17_LavaPits.lvl";
     	// Level 18
-    	gameLevelNames[18] = "18. Over The Falls";
-    	gameLevelFiles[18] = "data/gamelevels/18_OverTheFalls.lvl";
+    	gameLevelNames[19] = "18. Black Mountain (Day)";
+    	gameLevelFiles[19] = "data/gamelevels/18_BlackMountainDay.lvl";
     	// Level 19
-    	gameLevelNames[19] = "19. Absail";
-    	gameLevelFiles[19] = "data/gamelevels/19_Absail.lvl";
-    	// Level 20
-    	gameLevelNames[20] = "20. Trials";
-    	gameLevelFiles[20] = "data/gamelevels/20_Trials.lvl";
+    	gameLevelNames[20] = "19. Black Mountain (Night)";
+    	gameLevelFiles[20] = "data/gamelevels/19_BlackMountainNight.lvl";
+		// Level 20
+		gameLevelNames[21] = "20. Mwanda Peaks";
+		gameLevelFiles[21] = "data/gamelevels/20_MwandaPeaks.lvl";
     	// Level 21
-    	gameLevelNames[21] = "21. Lunatic";
-    	gameLevelFiles[21] = "data/gamelevels/21_Lunatic.lvl";
+    	gameLevelNames[22] = "21. Lunatic";
+    	gameLevelFiles[22] = "data/gamelevels/21_Lunatic.lvl";
     	// Level 22
-    	gameLevelNames[22] = "22. Multiverse";
-    	gameLevelFiles[22] = "data/gamelevels/22_Multiverse.lvl";
+    	gameLevelNames[23] = "22. Multiverse";
+    	gameLevelFiles[23] = "data/gamelevels/22_Multiverse.lvl";
     	// Level 23
-    	gameLevelNames[23] = "23. Escape Room";
-    	gameLevelFiles[23] = "data/gamelevels/23_EscapeRoom.lvl";
+    	gameLevelNames[24] = "23. Escape Room";
+    	gameLevelFiles[24] = "data/gamelevels/23_EscapeRoom.lvl";
     	// Level 24
-    	gameLevelNames[24] = "24. Barracks";
-    	gameLevelFiles[24] = "data/gamelevels/24_Barracks.lvl";
+    	gameLevelNames[25] = "24. Barracks";
+    	gameLevelFiles[25] = "data/gamelevels/24_Barracks.lvl";
     	// Level 25
-    	gameLevelNames[25] = "25. Hiraeth";
-    	gameLevelFiles[25] = "data/gamelevels/25_Hiraeth.lvl";
+    	gameLevelNames[26] = "25. Hiraeth";
+    	gameLevelFiles[26] = "data/gamelevels/25_Hiraeth.lvl";
     	// Level 26
-    	gameLevelNames[26] = "26. Asteroid";
-    	gameLevelFiles[26] = "data/gamelevels/26_Asteroid.lvl";
+    	gameLevelNames[27] = "26. Asteroid";
+    	gameLevelFiles[27] = "data/gamelevels/26_Asteroid.lvl";
     	// Level 27
-    	gameLevelNames[27] = "27. Dinosaur";
-    	gameLevelFiles[27] = "data/gamelevels/27_Dinosaur.lvl";
+    	gameLevelNames[28] = "27. Dinosaur";
+    	gameLevelFiles[28] = "data/gamelevels/27_Dinosaur.lvl";
     	// Level 28
-    	gameLevelNames[28] = "28. Gold Digger";
-    	gameLevelFiles[28] = "data/gamelevels/28_GoldDigger.lvl";
+    	gameLevelNames[29] = "28. Gold Digger";
+    	gameLevelFiles[29] = "data/gamelevels/28_GoldDigger.lvl";
     	// Level 29
-    	gameLevelNames[29] = "29. Free Will";
-    	gameLevelFiles[29] = "29_FreeWill.lvl";
+    	gameLevelNames[30] = "29. Free Will";
+    	gameLevelFiles[30] = "29_FreeWill.lvl";
     	// Level 30
-    	gameLevelNames[30] = "30. Short Supply";
-    	gameLevelFiles[30] = "data/gamelevels/30_ShortSupply.lvl";
+    	gameLevelNames[31] = "30. Short Supply";
+    	gameLevelFiles[31] = "data/gamelevels/30_ShortSupply.lvl";
     	// Level 31
-    	gameLevelNames[31] = "31. Ascraeus Mons";
-    	gameLevelFiles[31] = "data/gamelevels/31_AscraeusMons.lvl";
+    	gameLevelNames[32] = "31. Ascraeus Mons";
+    	gameLevelFiles[32] = "data/gamelevels/31_AscraeusMons.lvl";
     	// Level 32
-    	gameLevelNames[32] = "32. Rigor Mortis";
-    	gameLevelFiles[32] = "data/gamelevels/32_RigorMortis.lvl";
+    	gameLevelNames[33] = "32. Rigor Mortis";
+    	gameLevelFiles[33] = "data/gamelevels/32_RigorMortis.lvl";
     	// Level 33
-    	gameLevelNames[33] = "33. Lava Tube";
-    	gameLevelFiles[33] = "data/gamelevels/33_LavaTube.lvl";
+    	gameLevelNames[34] = "33. Lava Tube";
+    	gameLevelFiles[34] = "data/gamelevels/33_LavaTube.lvl";
     	// Level 34
-    	gameLevelNames[34] = "34. Olympus Mons";
-    	gameLevelFiles[34] = "data/gamelevels/34_OlympusMons.lvl";
+    	gameLevelNames[35] = "34. Olympus Mons";
+    	gameLevelFiles[35] = "data/gamelevels/34_OlympusMons.lvl";
     	// Level 35
-    	gameLevelNames[35] = "35. The Message";
-    	gameLevelFiles[35] = "data/gamelevels/35_TheMessage.lvl";
+    	gameLevelNames[36] = "35. The Message";
+    	gameLevelFiles[36] = "data/gamelevels/35_TheMessage.lvl";
     	// Level 36
-    	gameLevelNames[36] = "36. Adrift";
-    	gameLevelFiles[36] = "data/gamelevels/36_Adrift.lvl";
+    	gameLevelNames[37] = "36. Adrift";
+    	gameLevelFiles[37] = "data/gamelevels/36_Adrift.lvl";
     	// Level 37
-    	gameLevelNames[37] = "37. Emerald Harvest";
-    	gameLevelFiles[37] = "data/gamelevels/37_EmeraldHarvest.lvl";
+    	gameLevelNames[38] = "37. Emerald Harvest";
+    	gameLevelFiles[38] = "data/gamelevels/37_EmeraldHarvest.lvl";
     	// Level 38
-    	gameLevelNames[38] = "38. Alone";
-    	gameLevelFiles[38] = "data/gamelevels/38_Alone.lvl";
+    	gameLevelNames[39] = "38. Alone";
+    	gameLevelFiles[39] = "data/gamelevels/38_Alone.lvl";
     	// Level 39
-    	gameLevelNames[39] = "39. Solar System";
-    	gameLevelFiles[39] = "data/gamelevels/39_SolarSystem.lvl";
+    	gameLevelNames[40] = "39. Solar System";
+    	gameLevelFiles[40] = "data/gamelevels/39_SolarSystem.lvl";
     	// Level 40
-    	gameLevelNames[40] = "40. Dreaming of Home";
-    	gameLevelFiles[40] = "data/gamelevels/40_DreamingofHome.lvl";
+    	gameLevelNames[41] = "40. Dreaming of Home";
+    	gameLevelFiles[41] = "data/gamelevels/40_DreamingofHome.lvl";
 
     	// Finally, initialise the records strings
     	updateRecords();
--- gitit.sh
+++ gitit.sh
@@ -2,7 +2,6 @@ rm BikeWarp/core/assets/BGstate.dat
 rm BikeWarp/core/assets/WorldRecords.dat
 rm BikeWarp/core/assets/replays/*
 rmdir BikeWarp/core/assets/replays
-cp BikeWarp/desktop/build/libs/desktop-1.0.jar BikeWarp.jar
+cp BikeWarp/desktop/build/libs/desktop-1.0.jar ~/Desktop/BikeWarp/
 git commit * -m "update"
 git push
-cp BikeWarp.jar ~/Desktop/BikeWarp/
